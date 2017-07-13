512c3adf4be23889e9b229e276d436a701a5a8d9
duks <duks@robooot.com>
2017-07-13 10:58:33 +0800

    Initial Commit

---
+++ DKSTextFieldMove/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKSTextFieldMove
+//
+//  Created by aDu on 2017/7/13.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
